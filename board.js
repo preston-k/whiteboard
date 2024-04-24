73356c44a8a646376d2b722f5091865af287b027
Preston <71202878+preston-k@users.noreply.github.com>
2024-04-24 12:20:20 -0700

    1.3.2: Blocklist removed tempoarily

--- board.js
+++ board.js
@@ -131,7 +131,7 @@ document.querySelector('#createNew').addEventListener('click', createNew)
 document.querySelector('#close').addEventListener('click', () => {
   document.getElementById('newSubForm').style.display = 'none'
 })
-const blocklist = ['fuck', 'shit', 'ass', 'nigger']//, 'bitch', 'bastard', 'slut', 'dick', 'cunt', 'pussy', 'whore', 'fag', 'damn', 'douche', 'twat', 'cock', 'bollocks', 'arsehole', 'asshole', 'bugger', 'wanker', 'prick', 'tits', 'cum', 'boob', 'clit', 'fanny', 'piss', 'dildo', 'dyke', 'slag', 'motherfucker', 'goddamn', 'bint', 'bellend', 'muff', 'paki', 'chink', 'coon', 'kike', 'spic', 'faggot', 'poof', 'nonce', 'tranny', 'gringo', 'jizz', 'minge', 'nazi', 'bollock', 'bollox', 'shite', 'retard', 'cumshot', 'gook', 'beaner', 'skeet', 'tit', 'knob', 'felch', 'kunt', 'pecker', 'gooch', 'punani', 'taint', 'wang', 'wank', 'poon', 'schlong', 'fuckwit', 'poontang', 'gash', 'snatch', 'fuckboy', 'cumdump', 'shithead', 'fuckface', 'skank', 'hoe', 'bimbo', 'sod', 'coochie', 'thot', 'douchebag', 'arse'] // ChatGPT generated word list to block if these words get added to the submissions.
+const blocklist = []// Put words in that list to block them.
 
 async function submitSubmission() {
   if (document.querySelector('#submission').value == '') {
